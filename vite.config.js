4468e80d3cb9c7ad5bb1401c84778932b08b28d0
Irene MG <mendozagonzalez.irene@gmail.com>
2025-07-20 08:07:12 +0200

    Para publicar

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: './',
+    base: '/modern-art-gallery/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                location: resolve(__dirname, 'location.html'),
+            }
+        }
+    }
 })
